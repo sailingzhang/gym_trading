26b327165f628fa52ad308c42d3e5a821093d74e
sailingzhang <aa582885848@outlook.com>
2019-10-16 23:08:33 +0800

    add gym_anytrading_master

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 import gym
-import gym_anytrading
-from gym_anytrading.envs import TradingEnv, ForexEnv, StocksEnv, Actions, Positions 
-from gym_anytrading.datasets import FOREX_EURUSD_1H_ASK, STOCKS_GOOGL
+# import gym_anytrading
+# from gym_anytrading.envs import TradingEnv, ForexEnv, StocksEnv, Actions, Positions 
+# from gym_anytrading.datasets import FOREX_EURUSD_1H_ASK, STOCKS_GOOGL
+
+from gym_anytrading_master.gym_anytrading import datasets
+from gym_anytrading_master.gym_anytrading.envs import  ForexEnv
+
 import matplotlib.pyplot as plt
+from copy import deepcopy
 
-env = gym.make('forex-v0', frame_bound=(50, 100), window_size=10)
+# env = gym.make('forex-v0', frame_bound=(50, 100), window_size=10)
 # env = gym.make('stocks-v0', frame_bound=(50, 100), window_size=10)
-
+env = ForexEnv(df = deepcopy(datasets.FOREX_EURUSD_1H_ASK),window_size=24,frame_bound= (24, len(datasets.FOREX_EURUSD_1H_ASK)))
 observation = env.reset()
 while True:
     action = env.action_space.sample()
